9e5e96b671d9c5ad246c2a031b682d8a68f7f4b8
hong.viet <vietnh177@gmail.com>
2018-03-01 22:19:54 +0700

    Initial commit

---
+++ app/src/main/java/sec/com/hong/viet/orderfood/LoginActivity.java
@@ -0,0 +1,13 @@
+package sec.com.hong.viet.orderfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
